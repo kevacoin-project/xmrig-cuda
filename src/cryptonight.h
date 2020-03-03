e04e919003cb6c8c96043e7bf9cef93023f9e480
kevacoin <info@kevacoin.org>
2020-03-02 22:20:03 -0800

    Added RandomX_Keva.

--- src/cryptonight.h
+++ src/cryptonight.h
@@ -99,3 +99,4 @@ namespace RandomX_Arqma   { void hash(nvid_ctx *ctx, uint32_t nonce, uint64_t ta
 namespace RandomX_Loki    { void hash(nvid_ctx *ctx, uint32_t nonce, uint64_t target, uint32_t *rescount, uint32_t *resnonce, uint32_t batch_size); }
 namespace RandomX_Monero  { void hash(nvid_ctx *ctx, uint32_t nonce, uint64_t target, uint32_t *rescount, uint32_t *resnonce, uint32_t batch_size); }
 namespace RandomX_Wownero { void hash(nvid_ctx *ctx, uint32_t nonce, uint64_t target, uint32_t *rescount, uint32_t *resnonce, uint32_t batch_size); }
+namespace RandomX_Keva    { void hash(nvid_ctx *ctx, uint32_t nonce, uint64_t target, uint32_t *rescount, uint32_t *resnonce, uint32_t batch_size); }
